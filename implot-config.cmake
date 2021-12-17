8025238dcd9ff572039b3a5f2294dfb2b95e58b1
Robert Hilton <robert.a.hilton.jr@gmail.com>
2021-12-17 22:41:30 +0000

    Update

--- implot-config.cmake
+++ implot-config.cmake
@@ -1,3 +1,3 @@
 cmake_minimum_required(VERSION 3.8)
-set(IMGUI_INCLUDE_DIRS "/usr/local/include/implot")
-set(IMGUI_LIBRARIES "/usr/local/lib/implot.a")
+set(IMPLOT_INCLUDE_DIRS "/usr/local/include/implot")
+set(IMPLOT_LIBRARIES "/usr/local/lib/implot.a")
